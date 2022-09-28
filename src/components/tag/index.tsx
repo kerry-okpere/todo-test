fdb5dbee0edec76468a94106bf5651a9de4bf6fb
kerry-okpere <kerryokpere@gmail.com>
2022-09-28 18:28:01 +0100

    feat: create tag component

---
+++ src/components/tag/index.tsx
@@ -0,0 +1,5 @@
+import { StyledTag } from './styles'
+
+export const Tag = ({ text }: { text: string }) => {
+  return <StyledTag>{text}</StyledTag>
+}
---
+++ src/components/tag/styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const StyledTag = styled.span`
+  padding: 0px 5px;
+  border-radius: 5px;
+  background-color: #eeeff2;
+  color: #36454f;
+  font-size: 12px;
+  font-weight: bold;
+  max-width: 70px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  vertical-align: middle;
+`
