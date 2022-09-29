ef4749bbec28f7d704aef1049dc1ea7e39c155e4
kerry-okpere <kerryokpere@gmail.com>
2022-09-29 18:52:51 +0100

    feat: greet with dynamic emoji

--- src/components/greeting/index.tsx
+++ src/components/greeting/index.tsx
@@ -3,10 +3,14 @@ import { StyleHeading, StyleSubHeading, StyledHeader } from './styles'
 
 export const Greeting = () => {
   const { dayName, month, day, timeOfDay } = useTodayDate(new Date())
+  const emoji =
+    timeOfDay === 'morning' ? '⛅' : timeOfDay === 'afternoon' ? '☀️' : '🌒'
 
   return (
     <StyledHeader>
-      <StyleHeading>👋 Good {timeOfDay}!</StyleHeading>
+      <StyleHeading>
+        {emoji} Good {timeOfDay}!
+      </StyleHeading>
       <StyleSubHeading>
         It&apos;s {`${dayName}, ${month} ${day}`}
       </StyleSubHeading>
