659813b7198b18cbcad0c66d0bfa85a2409098b0
kerry-okpere <kerryokpere@gmail.com>
2022-09-29 15:36:48 +0100

    feat: minor ui update

--- src/components/greeting/index.tsx
+++ src/components/greeting/index.tsx
@@ -2,7 +2,6 @@ import { useTodayDate } from '@hooks/useDate'
 import { StyleHeading, StyleSubHeading, StyledHeader } from './styles'
 
 export const Greeting = () => {
-  // TODO: make date reactive per hour?
   const { dayName, month, day, timeOfDay } = useTodayDate(new Date())
 
   return (
--- src/components/greeting/styles.tsx
+++ src/components/greeting/styles.tsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components'
 
 export const StyleHeading = styled.h1`
-  color: ${(props) => props.theme.palette.gray['005']};
+  color: ${(props) => props.theme.palette.gray['007']};
   font-size: x-large;
   font-weight: 600;
   padding: 0.25rem 0;
 `
 
 export const StyleSubHeading = styled.p`
-  color: ${(props) => props.theme.palette.gray['005']};
+  color: ${(props) => props.theme.palette.gray['007']};
   font-size: large;
 `
 
-export const StyledHeader = styled.header`
+export const StyledHeader = styled.div`
   padding: 2rem 0;
 `
