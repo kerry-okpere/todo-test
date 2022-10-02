1ecc243fd1b8114ef85599abb16991cb9474fa7e
kerry-okpere <kerryokpere@gmail.com>
2022-10-02 18:45:42 +0100

    fix: allow html props

--- src/components/tag/index.tsx
+++ src/components/tag/index.tsx
@@ -1,5 +1,8 @@
 import { StyledTag } from './styles'
+interface Props extends React.HTMLProps<HTMLSpanElement> {
+  text: string
+}
 
-export const Tag = ({ text }: { text: string }) => {
+export const Tag = ({ text }: Props) => {
   return <StyledTag>{text}</StyledTag>
 }
